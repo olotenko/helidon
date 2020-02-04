60739a8b611d862b2dc5ba44b7ffa05b6b94d66f
Daniel Kec <daniel.kec@oracle.com>
2020-02-04 17:58:17 +0100

    Remove doSubscribe from BaseProcessor, Single#mapMany is served by FlatMapProcessor
    Signed-off-by: Daniel Kec <daniel.kec@oracle.com>

--- common/reactive/src/main/java/io/helidon/common/reactive/Single.java
+++ common/reactive/src/main/java/io/helidon/common/reactive/Single.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,6 +23,7 @@ import java.util.concurrent.Flow.Publisher;
 import java.util.concurrent.Flow.Subscriber;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.function.Function;
 
 import io.helidon.common.mapper.Mapper;
 
@@ -36,7 +37,7 @@ public interface Single<T> extends Subscribable<T> {
     /**
      * Map this {@link Single} instance to a new {@link Single} of another type using the given {@link Mapper}.
      *
-     * @param <U> mapped item type
+     * @param <U>    mapped item type
      * @param mapper mapper
      * @return Single
      * @throws NullPointerException if mapper is {@code null}
@@ -50,13 +51,29 @@ public interface Single<T> extends Subscribable<T> {
     /**
      * Map this {@link Single} instance to a publisher using the given {@link Mapper}.
      *
-     * @param <U> mapped items type
+     * @param <U>    mapped items type
+     * @param mapper mapper
+     * @return Publisher
+     * @throws NullPointerException if mapper is {@code null}
+     * @deprecated Use {@link Single#flatMap}
+     */
+    @Deprecated
+    default <U> Multi<U> mapMany(Mapper<T, Publisher<U>> mapper) {
+        var processor = SingleMultiMappingProcessor.<T, U>create().mapper(mapper::map);
+        this.subscribe(processor);
+        return processor;
+    }
+
+    /**
+     * Map this {@link Single} instance to a publisher using the given {@link Mapper}.
+     *
+     * @param <U>    mapped items type
      * @param mapper mapper
      * @return Publisher
      * @throws NullPointerException if mapper is {@code null}
      */
-    default <U> Multi<U> mapMany(Mapper<T, Publisher<U>> mapper) {
-        SingleMultiMappingProcessor<T, U> processor = new SingleMultiMappingProcessor<>(mapper);
+    default <U> Multi<U> flatMap(Function<T, Publisher<U>> mapper) {
+        var processor = SingleMultiMappingProcessor.<T, U>create().mapper(mapper);
         this.subscribe(processor);
         return processor;
     }
@@ -81,9 +98,10 @@ public interface Single<T> extends Subscribable<T> {
 
     /**
      * Short-hand for {@code  toFuture().toCompletableFuture().get()}.
+     *
      * @return T
      * @throws InterruptedException if the current thread was interrupted while waiting
-     * @throws ExecutionException if the future completed exceptionally
+     * @throws ExecutionException   if the future completed exceptionally
      */
     default T get() throws InterruptedException, ExecutionException {
         return toStage().toCompletableFuture().get();
@@ -91,12 +109,13 @@ public interface Single<T> extends Subscribable<T> {
 
     /**
      * Short-hand for {@code toFuture().toCompletableFuture().get()}.
+     *
      * @param timeout the maximum time to wait
-     * @param unit the time unit of the timeout argument
+     * @param unit    the time unit of the timeout argument
      * @return T
      * @throws InterruptedException if the current thread was interrupted while waiting
-     * @throws ExecutionException if the future completed exceptionally
-     * @throws TimeoutException if the wait timed out
+     * @throws ExecutionException   if the future completed exceptionally
+     * @throws TimeoutException     if the wait timed out
      */
     default T get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
         return toStage().toCompletableFuture().get(timeout, unit);
@@ -107,7 +126,7 @@ public interface Single<T> extends Subscribable<T> {
      * publisher publishes more than one item, the resulting {@link Single} will hold an error. Use {@link Multi#first()} instead
      * in order to get the first item of a publisher that may publish more than one item.
      *
-     * @param <T> item type
+     * @param <T>    item type
      * @param source source publisher
      * @return Single
      * @throws NullPointerException if source is {@code null}
@@ -126,7 +145,7 @@ public interface Single<T> extends Subscribable<T> {
     /**
      * Create a {@link Single} instance that publishes the given item to its subscriber(s).
      *
-     * @param <T> item type
+     * @param <T>  item type
      * @param item item to publish
      * @return Single
      * @throws NullPointerException if item is {@code null}
@@ -139,7 +158,7 @@ public interface Single<T> extends Subscribable<T> {
      * Create a {@link Single} instance that reports the given given exception to its subscriber(s). The exception is reported by
      * invoking {@link Subscriber#onError(java.lang.Throwable)} when {@link Publisher#subscribe(Subscriber)} is called.
      *
-     * @param <T> item type
+     * @param <T>   item type
      * @param error exception to hold
      * @return Single
      * @throws NullPointerException if error is {@code null}
